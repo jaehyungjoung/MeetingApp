bd61aa25f400f233eb95cf7e0cceda878f3effd7
sjjang0322 <sjjang0322@naver.com>
2019-12-11 15:13:59 +0900

    add ChatData(missing)

---
+++ app/src/main/java/com/example/meetingapp/ChatData.java
@@ -0,0 +1,34 @@
+package com.example.meetingapp;
+
+public class ChatData {
+    private String msg;
+    private String nickname;
+
+
+
+    public ChatData() {
+        this.msg = msg;
+        this.nickname = nickname;
+    }
+
+
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+
+
+}
